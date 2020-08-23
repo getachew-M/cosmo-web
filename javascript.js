849119f75d475aa835d246988d347b072e0ad5da
Getachew Molla <triplets3e@gmail.com>
2020-08-23 11:42:51 -0400

    Adding some features

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var repl = document.getElementsByClassName('.form-control')
+var messg='Thanks for contact us !'
+// repl.addEventListener('click', function(){
+//  return messg;
+//  console.log(messg);
+// })
+document.querySelector('.form-control').addEventListener('click',(messg) => {
+    return messg;
+    console.log(messg)
+});
